b6b48e887c756cef2c16068b27f4bafaae00f137
jun1rox <junior_alaniz@hotmail.com>
2022-06-19 01:19:43 -0300

    hook customizado atualizarEvento

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -1,12 +1,11 @@
-
 import React from 'react';
 import style from './Calendario.module.scss';
 import ptBR from './localizacao/ptBR.json';
 import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from 'kalend';
 import 'kalend/dist/styles/index.css';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { listaDeEventosState } from '../../state/atom';
-import { IEvento } from '../../interfaces/IEvento';
+import useAtualizarEvento from '../../state/hooks/useAtualizarEventos';
 
 interface IKalendEvento {
   id?: number;
@@ -20,7 +19,7 @@ const Calendario: React.FC = () => {
 
   const eventosKalend = new Map<string, IKalendEvento[]>();
   const eventos = useRecoilValue(listaDeEventosState);
-  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+  const atualizarEvento = useAtualizarEvento();
 
   eventos.forEach(evento => {
     const chave = evento.inicio.toISOString().slice(0, 10);
@@ -46,11 +45,7 @@ const Calendario: React.FC = () => {
       eventoAtualizado.inicio = new Date(kalendEventoAtualizado.startAt);
       eventoAtualizado.fim = new Date(kalendEventoAtualizado.endAt);
 
-      setListaDeEventos(listaAntiga => {
-        const indice = listaAntiga.findIndex(evt => evt.id === evento.id);
-
-        return [...listaAntiga.slice(0, indice), eventoAtualizado, ...listaAntiga.slice(indice + 1)];
-      })
+      atualizarEvento(eventoAtualizado);
     }
   };
 
@@ -73,7 +68,3 @@ const Calendario: React.FC = () => {
 }
 
 export default Calendario;
-
-function setListaDeEventos(arg0: (listaAntiga: any) => any[]) {
-  throw new Error('Function not implemented.');
-}
